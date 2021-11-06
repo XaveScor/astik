0cb212e5dc76370b50426ea0fd867ed228532bf2
Andrey Vassilyev <xavescor@gmail.com>
2021-11-07 02:51:49 +0300

    initial commit

---
+++ src/parser/tokenize.ts
@@ -0,0 +1,40 @@
+type Token = string
+type TokenizedDocument = ReadonlyArray<Token>
+
+const operators = ['+', '-', '='];
+function isDelimiter(ch: string): boolean {
+    if ('0' <= ch && ch <= '9') {
+        return false;
+    }
+
+    if ('a' <= ch && ch <= 'z') {
+        return false;
+    }
+
+    if ('A' <= ch && ch <= 'Z') {
+        return false;
+    }
+
+    if (operators.includes(ch)) {
+        return false;
+    }
+
+    return true;
+}
+
+export function tokenize(input: string): TokenizedDocument {
+    let tokenAcc = '';
+    const res = [];
+    for (const ch of input) {
+        if (isDelimiter(ch)) {
+            if (tokenAcc !== '') {
+                res.push(tokenAcc);
+            }
+            tokenAcc = '';
+        } else {
+            tokenAcc += ch;
+        }
+    }
+
+    return res;
+}
